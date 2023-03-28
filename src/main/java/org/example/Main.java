a5873e1a2a59151640310f61d60e5b7dc47a9194
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-28 15:43:45 +0300

    new line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         /*teach veronika to commit and push them*/
+        System.out.println("Hello world!");
     }
 }
