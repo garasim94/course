7f644328a617af0b4304136cf7d03c2db4b92cf2
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-28 15:42:25 +0300

    teach veronika

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        /**/
+        /*teach veronika to commit and push them*/
     }
 }
