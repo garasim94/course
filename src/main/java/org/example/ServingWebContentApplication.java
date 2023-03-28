beb3b9e2810b7ee30dcbfeb134dd0feed50241a3
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-29 01:38:09 +0300

    creat ServingWebContentApplication

---
+++ src/main/java/org/example/ServingWebContentApplication.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
