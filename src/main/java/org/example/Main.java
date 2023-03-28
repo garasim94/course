2fb62b7573f98c8116176ab0f5b198218c5c4e01
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-28 15:51:30 +0300

    DataBase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        /*teach veronika to commit and push them*/
-        System.out.println("Hello world!");
     }
 }
